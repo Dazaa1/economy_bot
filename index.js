1dbfa94b4ceba9aa7dc052228a59a0ec9394820a
Dazaa1 <marouanelankaoui066@gmail.com>
2026-04-01 17:40:02 +0100

    Initial code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Working");
+// requiring necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// creating a client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+})
+
+client.login(token);
