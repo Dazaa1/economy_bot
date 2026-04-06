8ef845e2edd2c963f37e13a4cc19466190602787
Dazaa1 <marouanelankaoui066@gmail.com>
2026-04-06 08:35:20 +0100

    refactor work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -7,20 +7,15 @@ function getRandomIntInclusive(min, max) {
 }
 
 module.exports = {
-    cooldown: 14400,
+    cooldown: 1800,
     data: new SlashCommandBuilder().setName("work").setDescription("Earn coins"),
     async execute(interaction) {
         const { id, username } = interaction.user;
 
-        // 1. Ensure user exists
-        db.db.upsertUser.run({ id, username }); // Note: you exported as { db: db }, so it might be db.db depending on your require
-
+        db.db.upsertUser.run({ id, username });
         const coinsEarned = getRandomIntInclusive(10, 100);
         
-        // 2. Perform the update first
         db.db.addCoins.run(coinsEarned, id);
-
-        // 3. Get the FRESH data from the database
         const updatedUser = db.db.getUser.get(id);
 
         await interaction.reply(
